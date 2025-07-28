b48be6f901cf5fec6e51d84e9a7bc1420c58540a
voidgiux <gfinotti@pm.me>
2025-07-28 13:53:45 +0200

    refactoring main

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pygame
 from constants import *
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
-        print("Starting Asteroids!")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
-        screen.fill(SCREEN_COLOR, rect=None, special_flags=0) 
-        pygame.display.flip()
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return 
+                return
+
+        screen.fill("black")
+        pygame.display.flip()    
+
 
 if __name__ == "__main__":
     main()
