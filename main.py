7e191ebd90e565f97d3efd32a225dfa19dc78e41
voidgiux <gfinotti@pm.me>
2025-07-28 16:45:37 +0200

    implementation of clock

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+        
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +16,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()    
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
