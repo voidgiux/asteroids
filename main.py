2a2d7c82c1da13ffa26f276320b5c34878c0fbb5
voidgiux <gfinotti@pm.me>
2025-07-28 17:05:46 +0200

    implemented class Player

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-        
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
-        pygame.display.flip()    
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+
