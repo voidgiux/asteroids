0b35d8d668b40dec25975935dd82d5eacdf31681
voidgiux <gfinotti@pm.me>
2025-07-28 18:38:07 +0200

    implemented collision for shots on asteroids

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.kill()
+
         screen.fill("black")
 
         for obj in drawable:
